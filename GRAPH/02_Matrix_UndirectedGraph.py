b7f88ddbde460cb103c6c8b27e935ae5f9bd84d0
Anshuman-X <anshumanacharya51@gmail.com>
2026-01-18 17:32:27 +0530

    Added matrix representation of undirected graph

---
+++ GRAPH/02_Matrix_UndirectedGraph.py
@@ -0,0 +1,22 @@
+class Graph:
+    def __init__(self,vertex):
+        self.mat = [[0]*vertex for x in range(vertex)]
+        self.size = vertex
+
+    def add_edge(self,src,dest):
+        if(0 <= src < self.size and 0 <= dest < self.size):
+            self.mat[src][dest] = 1
+            self.mat[dest][src] = 1
+        else:
+            print("Invalid Edge")
+            
+    def display(self):
+        for row in self.mat:
+            print(' '.join(map(str,row)))
+            
+G = Graph(5)
+G.add_edge(1,2)
+G.add_edge(1,4)
+G.add_edge(2,3)
+
+G.display()
