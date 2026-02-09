fd6db78b19382a5ed8af0a53893a090d237e1165
Anshuman-X <anshumanacharya51@gmail.com>
2026-02-10 00:04:38 +0530

    optimised the leetcode problem

--- ARRAY/find_target.py
+++ ARRAY/find_target.py
@@ -1,18 +1,23 @@
-def FindTarget(nums,target):
-    n = len(nums)
+# def FindTarget(nums,target):
+#     n = len(nums)
     
-    for i in range(n):
-        if nums[i]==target:
+#     for i in range(n):
+#         if nums[i]==target:
+#             return i
+        
+#         elif nums[i]>target:
+#             return i 
+        
+#     else:
+#         nums.append(target)
+#         return nums.index(target)
+        
+        
+def FindTarget(nums, target):
+    for i in range(len(nums)):
+        if nums[i] >= target:
             return i
-        
-        elif nums[i]>target:
-            return i 
-        
-    else:
-        nums.append(target)
-        return nums.index(target)
-        
-        
-        
+    return len(nums)
+
 nums= [1,3,5,6]
-print(FindTarget(nums,7))
+print(FindTarget(nums,5))
