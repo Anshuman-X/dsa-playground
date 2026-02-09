a8e6e221ac2b5054b18ac44d80031ea0bd6a0373
Anshuman-X <anshumanacharya51@gmail.com>
2026-02-09 23:59:37 +0530

    Added a leetcode problem

---
+++ ARRAY/find_target.py
@@ -0,0 +1,18 @@
+def FindTarget(nums,target):
+    n = len(nums)
+    
+    for i in range(n):
+        if nums[i]==target:
+            return i
+        
+        elif nums[i]>target:
+            return i 
+        
+    else:
+        nums.append(target)
+        return nums.index(target)
+        
+        
+        
+nums= [1,3,5,6]
+print(FindTarget(nums,7))
